f6480aecb8d64a644b5ca350838bd5c7d38cbd6f
collcorr72 <collcorr72@gmail.com>
2019-02-28 13:15:12 -0500

    I had to do this

--- step1/step2/step3/deck.py
+++ step1/step2/step3/deck.py
@@ -44,3 +44,28 @@ shuffle(players[1])
 for i in range(2):
     print("Hand %d:" % (i), players[i])
     
+def switch(argument):
+    switcher = {
+        "A": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "10": 10,
+        "J": 11,
+        "Q": 12,
+        "K": 13
+        }
+    if argument[:-1] in switcher.keys():
+        return switcher.get(argument[:-1])
+    else:
+        return "Invalid card!"
+    
+def compare_card(p1Card, p2Card):
+    ### STEP 3
+    
+    ### END STEP 3

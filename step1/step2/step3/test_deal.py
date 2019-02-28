3bbbed0dea370e8c1003f7fd285b73b6bdd3dca6
meowskers <wallie@rpi.edu>
2019-02-28 12:42:03 -0500

    Added 5 new test cases to test_deal
    Added 5 test cases:
    test_to_empty - Deals one card to an empty deck
    test_one_to_one - Deals one card to a deck with one card
    test_two_to_one - Deals two cards to a deck with one card
    test_two_to_empty - Deals two cards to an empty deck
    test_three_decks - Deals two decks to an empty deck

--- step1/step2/step3/test_deal.py
+++ step1/step2/step3/test_deal.py
@@ -1,13 +1,49 @@
 import unittest
-from deck import shuffle
+from deck import deal
+
 class MyTest(unittest.TestCase):
-	def setUp(self):
-		pass
-	#Checks to see if two cards are shuffled by making sure they are not the exact same at least once out of 30 tries
-	def test_(self):
-
-
-		
-
+    def setUp(self):
+        pass
+    #Deals one card to an empty deck 
+    def test_to_empty(self):
+        deck1 = ["A"]
+        deck2 = list()
+        deal(deck1,deck2)
+        self.assertEquals(deck1,[])
+        self.assertEquals(deck2,["A"])
+    #Deals one card to an deck with one card 
+    def test_one_to_one(self):
+        deck1 = ["B"]
+        deck2 = ["A"]
+        deal(deck1,deck2)
+        self.assertEquals(deck1,[])
+        self.assertEquals(deck2,["A","B"])
+    #Deals two cards to a deck with one card 
+    def test_two_to_one(self):
+        deck1 = ["B","C"]
+        deck2 = ["A"]
+        deal(deck1,deck2)
+        deal(deck1,deck2)
+        self.assertEquals(deck1,[])
+        self.assertEquals(deck2,["A","B","C"])
+    #Deals two cards to an empty deck 
+    def test_two_to_empty(self):
+        deck1 = ["A","B"]
+        deck2 = list()
+        deal(deck1,deck2)
+        deal(deck1,deck2)
+        self.assertEquals(deck1,[])
+        self.assertEquals(deck2,["A","B"])
+    #Deals from two decks into a third
+    def test_three_decks(self):
+        deck1 = ["A","C"]
+        deck2 = ["B"]
+        deck3 = list()
+        deal(deck1,deck3)
+        deal(deck2,deck3)
+        deal(deck1,deck3)
+        self.assertEquals(deck1,[])
+        self.assertEquals(deck2,[])
+        self.assertEquals(deck3,["A","B","C"])
 if __name__ == '__main__':
 	unittest.main()

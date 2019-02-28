3fc317d36da5b3c3d36dc6b24a3e7cdd917ee100
meowskers <wallie@rpi.edu>
2019-02-28 11:30:56 -0500

    Changed the structure of code deck in files so that the functions can be used in test files easily
    changed the structure of code so that it can be tested easily changed name of test so it can have mutiple tests
    also renamed test and added a new one to test the deal function

---
+++ step1/step2/step3/test_deal.py
@@ -0,0 +1,13 @@
+import unittest
+from deck import shuffle
+class MyTest(unittest.TestCase):
+	def setUp(self):
+		pass
+	#Checks to see if two cards are shuffled by making sure they are not the exact same at least once out of 30 tries
+	def test_(self):
+
+
+		
+
+if __name__ == '__main__':
+	unittest.main()

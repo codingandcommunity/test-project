8ab1d494ab320ac8b6034cec4fb2a5f78627bf85
meowskers <wallie@rpi.edu>
2019-03-18 13:18:13 -0400

    Took out print statment and added a Full_Deck test
    Took out print statment in test_ThreeCards
    
    Added test_Full_Deck which does the same as two deck test but with and enitre deck 10 times

--- step1/step2/test/test_shuffle.py
+++ step1/step2/test/test_shuffle.py
@@ -10,18 +10,12 @@ class MyTest(unittest.TestCase):
     def test_TwoCards(self):
         deck = ["A", "B"]
         notDifferent = True
-        a = 0 
-        b = 0
         i = 0
-        while(i<1000):
+        while(notDifferent and i<1000):
             if(d.shuffle(deck) != ["A", "B"]):
-                a = a + 1
                 notDifferent = False
-            else:
-                b = b + 1 
             i += 1
         self.assertEquals(notDifferent,False)
-        print("a: " +str(a)+ "    b: " + str(b))
         #Checks to see if the deck items are altered 
         deck.sort()
         self.assertEquals(deck, ["A","B"])
@@ -51,13 +45,26 @@ class MyTest(unittest.TestCase):
         for j in range(6):
         	if(combos[j]<50 or combos[j]>282):
         	    inRange = False
-        for r in range(6):
-            print( str(combos[r]) + "<1400")
         self.assertEquals(inRange, True)
         #Checks to see if the deck items are altered 
         deck.sort()
         self.assertEquals(deck, ["A","B","C"])
-
+    def test_Full_deck(self):
+        deck = ["AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
+        "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
+        "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
+        "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD"]
+        freshDeck = ["AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
+        "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
+        "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
+        "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD"]
+        notDifferent = True
+        i = 0 
+        while(notDifferent and i<10):
+            if(d.shuffle(deck) != freshDeck):
+                notDifferent = False
+            i += 1
+        self.assertEquals(notDifferent,False)
         
 
 if __name__ == '__main__':

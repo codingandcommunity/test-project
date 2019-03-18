6b4b1ff22f7280663a13d945450e493c51868b27
meowskers <wallie@rpi.edu>
2019-03-18 12:05:08 -0400

    Had to remove test with seed because it only works if the random function is run the same amount of times.
    Going to switch the other two shuffle tests so that all posibliites must be equally shown

--- step1/step2/test/test_shuffle.py
+++ step1/step2/test/test_shuffle.py
@@ -1,46 +1,55 @@
 import unittest
 import sys
 import random
-random.seed(2)
 sys.path.insert(0,"..")
 import deck as d
 class MyTest(unittest.TestCase):
-	def setUp(self):
-		pass
-	#Checks to see if two cards are shuffled by making sure they are not the exact same at least once out of 30 tries
-	def test_TwoCards(self):
-		deck = ["A", "B"]
-		notDifferent = True
-		i = 0
-		while(notDifferent and i<30):
-			if(d.shuffle(deck) != ["A", "B"]):
-				notDifferent = False
-			i += 1
-		self.assertEquals(notDifferent,False)
-		#Checks to see if the deck items are altered 
-		deck.sort()
-		self.assertEquals(deck, ["A","B"])
-	#Checks to see if three cards are shuffled by making sure they are not the exact same at least once out of 40 tries
-	def test_ThreeCards(self):
-		deck = ["A", "B", "C"]
-		notDifferent = True
-		i = 0
-		while(notDifferent and i<40):
-			if(d.shuffle(deck) != ["A", "B", "C"]):
-				notDifferent = False
-			i += 1
-		self.assertEquals(notDifferent,False)
-		#Checks to see if the deck items are altered 
-		deck.sort()
-		self.assertEquals(deck, ["A","B","C"])
-	#Using a random seed checks to see if shuffle works
-	def test_random(self):
-	    deck = ["A", "B", "C", "D", "E", "F"]
-	    temp = ['B' , 'D', 'F', 'E', 'C', 'A']
-	    d.shuffle(deck)
-	    self.assertEquals(deck,temp )
+    def setUp(self):
+        pass
+    #Checks to see if two cards are shuffled by making sure they are not the exact same at least once out of 30 tries
+    def test_TwoCards(self):
+        deck = ["A", "B"]
+        notDifferent = True
+        a = 0 
+        b = 0
+        i = 0
+        while(i<1000):
+            if(d.shuffle(deck) != ["A", "B"]):
+                a = a + 1
+                notDifferent = False
+            else:
+                b = b + 1 
+            i += 1
+        self.assertEquals(notDifferent,False)
+        print("a: " +str(a)+ "    b: " + str(b))
+        #Checks to see if the deck items are altered 
+        deck.sort()
+        self.assertEquals(deck, ["A","B"])
+    #Checks to see if three cards are shuffled by making sure they are not the exact same at least once out of 40 tries
+    #Prove that all combinations are equal
+    def test_ThreeCards(self):
+        deck = ["A", "B", "C"]
+        notDifferent = True
+        i = 0
+        a = 0
+        b = 0
+        c = 0
+        while(i<1000):
+            if(d.shuffle(deck) != ["A", "B", "C"]):
+                notDifferent = False
+                a = a + 1
+            if(d.shuffle(deck) == ["B", "A", "C"]):
+            	b = b +1
+            if(d.shuffle(deck) == ["C", "A", "B"]):
+                c = c+1
+            i += 1
+        print("a: " +str(a)+ "    b: " + str(b)+ "    c:"+str(c))
+        self.assertEquals(notDifferent,False)
+        #Checks to see if the deck items are altered 
+        deck.sort()
+        self.assertEquals(deck, ["A","B","C"])
 
-		
+        
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()

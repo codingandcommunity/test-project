6b042c04058b00756a4db624d2d1560dd6d2f8d0
meowskers <wallie@rpi.edu>
2019-03-19 11:20:34 -0400

    added full deck except one
    deals one deck to another except for one card testing in case they are just compying

--- step1/step2/step3/test/test_deal.py
+++ step1/step2/step3/test/test_deal.py
@@ -62,6 +62,21 @@ class MyTest(unittest.TestCase):
             deal(deck,deck2)
         self.assertEquals(freshDeck,deck2)
         self.assertEquals(deck,[])
+    #Deals most of a full deck 
+    def test_full_deck_except_one(self):
+        freshishDeck = ["AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
+        "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
+        "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
+        "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD"]
+        deck = ["AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
+        "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
+        "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
+        "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD"]
+        deck2 = list()
+        for i in range(len(deck)-1):
+            deal(deck,deck2)
+        self.assertEquals(freshishDeck,deck2)
+        self.assertEquals(deck,["KD"])
     
 if __name__ == '__main__':
 	unittest.main()

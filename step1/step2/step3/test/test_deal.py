041cd55379fadc481b8f62e7a6c9d92f03dc8be5
meowskers <wallie@rpi.edu>
2019-03-19 11:15:57 -0400

    Made it so it can test in another folder and added new test
    Added sys and other things so it can access the file deck.py to test
    
    Added new test entire deck where it deals one entire deck to another

--- step1/step2/step3/test/test_deal.py
+++ step1/step2/step3/test/test_deal.py
@@ -1,4 +1,6 @@
 import unittest
+import sys
+sys.path.insert(0,"..")
 from deck import deal
 
 class MyTest(unittest.TestCase):
@@ -45,5 +47,21 @@ class MyTest(unittest.TestCase):
         self.assertEquals(deck1,[])
         self.assertEquals(deck2,[])
         self.assertEquals(deck3,["A","B","C"])
+    #Deals an entire deck to another 
+    def test_full_deck(self):
+        freshDeck = ["AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
+        "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
+        "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
+        "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD"]
+        deck = ["AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
+        "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
+        "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
+        "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD"]
+        deck2 = list()
+        for i in range(len(deck)):
+            deal(deck,deck2)
+        self.assertEquals(freshDeck,deck2)
+        self.assertEquals(deck,[])
+    
 if __name__ == '__main__':
 	unittest.main()

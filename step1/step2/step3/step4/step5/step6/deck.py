3f7d2fb2115bed2fe27ed13c88a979fdc36ddf8d
collcorr72 <collcorr72@gmail.com>
2019-04-09 16:12:00 -0400

    Fixed main()

--- step1/step2/step3/step4/step5/step6/deck.py
+++ step1/step2/step3/step4/step5/step6/deck.py
@@ -194,12 +194,12 @@ def play_game():
             return 2
         elif len(players[1]) == 0:
             return 1
-        ### END STEP 5
+    ### END STEP 5
     
     
 if __name__ == '__main__':
 	# Starter Code 1
-    '''
+    
     deck = ["AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
         "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
         "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
@@ -224,28 +224,10 @@ if __name__ == '__main__':
     print("Shuffled:")
     for i in range(2):
         print("Hand %d:" % (i), players[i])
-    '''
     
-    '''
+    
     winner = play_game()
     if winner == 1:
         print("Player 1 won!")
     elif winner == 2:
-        print("Player2 won!")
-    '''
-    
-    players = list()
-    players.append(list())
-    players.append(list())
-    players[0] = ["KS", "QH", "5S"]
-    players[1] = ["2D", "6C", "6H"]
-    
-    
-    winner = play_game()
-    if winner == 1:
-        print("Player2 won!")
-    elif winner == 2:
-        print("Player1 won!")  
-    
-    
-    
+        print("Player2 won!") 

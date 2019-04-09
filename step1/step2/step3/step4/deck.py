9eceddffa5eb4f3c2a9ba77a6b920241f88145be
collcorr72 <collcorr72@gmail.com>
2019-04-09 16:41:40 -0400

    Added print_table()
    Because print_table() is used in turn(), I decided to include it in the code given beforehand.

--- step1/step2/step3/step4/deck.py
+++ step1/step2/step3/step4/deck.py
@@ -47,6 +47,52 @@ def compare_card(p1Card, p2Card):
         return 3    
     ### END STEP 3
     
+def print_table(hand1card, hand2card):
+    if hand1card[-1] == "C":
+        suit = "\u2663"
+    elif hand1card[-1] == "S":
+        suit = "\u2660"
+    elif hand1card[-1] == "D":
+        suit = "\u2666"
+    elif hand1card[-1] == "H":
+        suit = "\u2665"
+        
+    if hand2card[-1] == "C":
+        suit2 = "\u2663"
+    elif hand2card[-1] == "S":
+        suit2 = "\u2660"
+    elif hand2card[-1] == "D":
+        suit2 = "\u2666"
+    elif hand2card[-1] == "H":
+        suit2 = "\u2665"
+        
+    if len(hand1card[:-1]) == 1 and len(hand2card[:-1]) == 1:
+        topstring = ("| {}       |         | {}       |").format(hand1card[:-1], hand2card[:-1])
+    elif len(hand1card[:-1]) == 2 and len(hand2card[:-1]) == 1:
+        topstring = ("| {}      |         | {}       |").format(hand1card[:-1], hand2card[:-1])
+    elif len(hand1card[:-1]) == 1 and len(hand2card[:-1]) == 2:
+        topstring = ("| {}       |         | {}      |").format(hand1card[:-1], hand2card[:-1])
+    elif len(hand1card[:-1]) == 2 and len(hand2card[:-1]) == 2:
+        topstring = ("| {}      |         | {}      |").format(hand1card[:-1], hand2card[:-1])
+        
+    if len(hand1card[:-1]) == 1 and len(hand2card[:-1]) == 1:
+        botstring = ("|       {} |         |       {} |").format(hand1card[:-1], hand2card[:-1])
+    elif len(hand1card[:-1]) == 2 and len(hand2card[:-1]) == 1:
+        botstring = ("|      {} |         |       {} |").format(hand1card[:-1], hand2card[:-1])
+    elif len(hand1card[:-1]) == 1 and len(hand2card[:-1]) == 2:
+        botstring = ("|        {}|         |      {} |").format(hand1card[:-1], hand2card[:-1])
+    elif len(hand1card[:-1]) == 2 and len(hand2card[:-1]) == 2:
+        botstring = ("|      {} |         |      {} |").format(hand1card[:-1], hand2card[:-1])
+        
+    d = "\u2500"
+    print("\u250C",d,d,d,d,"\u2510"+"         "+"\u250C",d,d,d,d,"\u2510")
+    print(topstring)
+    print("|         |         |         |")
+    print("|    {}    |   VS.   |    {}    |".format(suit, suit2))
+    print("|         |         |         |")
+    print(botstring)
+    print("\u2514",d,d,d,d,"\u2518"+"         "+"\u2514",d,d,d,d,"\u2518")    
+    
 def turn(p1hand, p2hand):
     ### STEP 4
     pass
